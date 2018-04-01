036f1be359021d906760e7aad643169d383be1fa
Rainer Erich Scheichelbauer <mekka@mekkablue.com>
2018-04-01 16:36:51 +0200

    Initial Release

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # RandomRotate
-Glyphs.app filter for randomly rotating glyphs around their center.
+
+Glyphs.app filter for randomly rotating glyphs around their center. After installation, trigger the filter with *Filter > Random Rotate,* enter a number for the maximum rotation angle, and apply the value:
+
+![RandomRotate](RandomRotate.png)
+
+### Custom Parameter
+
+Use the gear menu in the lower left corner to copy a custom parameter with the current settings. You can then proceed to paste it in *File > Font Info > Instances,* in the *Custom Parameters* field of a listed font instance. At the end of the parameter value, add `; exclude:` or `; include:`, followed by a comma-separated list of glyph names, for excluding the indicated glyphs at export time, or limiting the filter’s effect to the glyphs, respectively. Example:
+
+```
+RandomRotate; maxAngle:40.0; exclude: a,b,c,adieresis
+```
+
+### Installation
+
+1. In *Window > Plugin Manager,* look for *RandomRotate.*
+2. Click the *Install* button next to its entry.
+3. Restart Glyphs.
+
+### Requirements
+
+The plugin works in Glyphs 2.5 in High Sierra. I can only test it in current app and OS versions, and perhaps it works on earlier versions too.
+
+### License
+
+Copyright 2018 Rainer Erich Scheichelbauer (@mekkablue).
+Based on sample code by Georg Seifert (@schriftgestalt) and Jan Gerner (@yanone).
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+See the License file included in this repository for further details.
---
+++ RandomRotate.glyphsFilter/Contents/Resources/plugin.py
@@ -0,0 +1,133 @@
+# encoding: utf-8
+
+###########################################################################################################
+#
+#
+#	Filter with dialog Plugin
+#
+#	Read the docs:
+#	https://github.com/schriftgestalt/GlyphsSDK/tree/master/Python%20Templates/Filter%20with%20Dialog
+#
+#	For help on the use of Interface Builder:
+#	https://github.com/schriftgestalt/GlyphsSDK/tree/master/Python%20Templates
+#
+#
+###########################################################################################################
+
+import objc
+from math import tan, radians
+from random import random
+from GlyphsApp import *
+from GlyphsApp.plugins import *
+
+def centerOfRect(rect):
+	"""
+	Returns the center of NSRect rect as an NSPoint.
+	"""
+	x = rect.origin.x + rect.size.width * 0.5
+	y = rect.origin.y + rect.size.height * 0.5
+	return NSPoint(x,y)
+
+def transform(shiftX=0.0, shiftY=0.0, rotate=0.0, skew=0.0, scale=1.0):
+	"""
+	Returns an NSAffineTransform object for transforming layers.
+	Apply an NSAffineTransform t object like this:
+		Layer.transform_checkForSelection_doComponents_(t,False,True)
+	Access its transformation matrix like this:
+		tMatrix = t.transformStruct() # returns the 6-float tuple
+	Apply the matrix tuple like this:
+		Layer.applyTransform(tMatrix)
+		Component.applyTransform(tMatrix)
+		Path.applyTransform(tMatrix)
+	Chain multiple NSAffineTransform objects t1, t2 like this:
+		t1.appendTransform_(t2)
+	"""
+	myTransform = NSAffineTransform.transform()
+	if rotate:
+		myTransform.rotateByDegrees_(rotate)
+	if scale != 1.0:
+		myTransform.scaleBy_(scale)
+	if not (shiftX == 0.0 and shiftY == 0.0):
+		myTransform.translateXBy_yBy_(shiftX,shiftY)
+	if skew:
+		skewStruct = NSAffineTransformStruct()
+		skewStruct.m11 = 1.0
+		skewStruct.m22 = 1.0
+		skewStruct.m21 = tan(radians(skew))
+		skewTransform = NSAffineTransform.transform()
+		skewTransform.setTransformStruct_(skewStruct)
+		myTransform.appendTransform_(skewTransform)
+	return myTransform
+
+
+class RandomRotate(FilterWithDialog):
+	
+	# Definitions of IBOutlets
+	
+	# The NSView object from the User Interface. Keep this here!
+	dialog = objc.IBOutlet()
+	
+	# Text field in dialog
+	maxAngleField = objc.IBOutlet()
+	
+	def settings(self):
+		self.menuName = Glyphs.localize({
+			'en': u'Random Rotate',
+			'de': u'Zufallsrotation',
+		})
+		
+		# Word on Run Button (default: Apply)
+		self.actionButtonLabel = Glyphs.localize({
+			'en': u'Rotate',
+			'de': u'Rotieren',
+		})
+		
+		# Load dialog from .nib (without .extension)
+		self.loadNib('IBdialog', __file__)
+	
+	# On dialog show
+	def start(self):
+		
+		# Set default value
+		Glyphs.registerDefault('com.mekkablue.RandomRotate.maxAngle', 15.0)
+		if Glyphs.defaults['com.mekkablue.RandomRotate.maxAngle'] == "GlyphsToolHand": # circumvent bug in API
+			Glyphs.defaults['com.mekkablue.RandomRotate.maxAngle'] = 15.0
+		
+		# Set value of text field
+		self.maxAngleField.setStringValue_(Glyphs.defaults['com.mekkablue.RandomRotate.maxAngle'])
+		
+		# Set focus to text field
+		self.maxAngleField.becomeFirstResponder()
+		
+	# Action triggered by UI
+	@objc.IBAction
+	def setMaxAngle_( self, sender ):
+		# Store value coming in from dialog
+		Glyphs.defaults['com.mekkablue.RandomRotate.maxAngle'] = sender.floatValue()
+		# Trigger redraw
+		self.update()
+	
+	# Actual filter
+	def filter(self, layer, inEditView, customParameters):
+		
+		# Called on font export, get value from customParameters
+		if customParameters.has_key('maxAngle'):
+			maxAngle = customParameters['maxAngle']
+		
+		# Called through UI, use stored value
+		else:
+			maxAngle = float(Glyphs.defaults['com.mekkablue.RandomRotate.maxAngle'])
+		
+		rotationAngle = -maxAngle + 2*maxAngle*random()
+		centerPoint = centerOfRect(layer.bounds)
+		rotateLayerAroundItsCenter = transform(shiftX=-centerPoint.x, shiftY=-centerPoint.y)
+		rotateLayerAroundItsCenter.appendTransform_( transform(rotate=rotationAngle) )
+		rotateLayerAroundItsCenter.appendTransform_( transform(shiftX=centerPoint.x, shiftY=centerPoint.y) )
+		layer.transform_checkForSelection_doComponents_( rotateLayerAroundItsCenter, False, True )
+	
+	def generateCustomParameter( self ):
+		return "%s; maxAngle:%s;" % (self.__class__.__name__, Glyphs.defaults['com.mekkablue.RandomRotate.maxAngle'] )
+	
+	def __file__(self):
+		"""Please leave this method unchanged"""
+		return __file__
