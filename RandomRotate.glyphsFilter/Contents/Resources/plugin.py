cc1116fcb60abddd61a499e73fe3c57f69f5d246
schriftgestalt <georg.seifert@mac.com>
2026-01-06 13:39:17 +0100

    cleanup

--- RandomRotate.glyphsFilter/Contents/Resources/plugin.py
+++ RandomRotate.glyphsFilter/Contents/Resources/plugin.py
@@ -16,11 +16,10 @@ from __future__ import division, print_function, unicode_literals
 ###########################################################################################################
 
 import objc
-from math import tan, radians
 from random import random
-from GlyphsApp import *
-from GlyphsApp.plugins import *
-
+from GlyphsApp import Glyphs
+from GlyphsApp.plugins import FilterWithDialog
+from Foundation import NSAffineTransform, NSPoint
 
 @objc.python_method
 def centerOfRect(rect):
@@ -29,7 +28,7 @@ def centerOfRect(rect):
 	"""
 	x = rect.origin.x + rect.size.width * 0.5
 	y = rect.origin.y + rect.size.height * 0.5
-	return NSPoint(x,y)
+	return NSPoint(x, y)
 
 
 @objc.python_method
@@ -51,12 +50,12 @@ def transform(shiftX=0.0, shiftY=0.0, rotate=0.0):
 	if rotate:
 		myTransform.rotateByDegrees_(rotate)
 	if not (shiftX == 0.0 and shiftY == 0.0):
-		myTransform.translateXBy_yBy_(shiftX,shiftY)
+		myTransform.translateXBy_yBy_(shiftX, shiftY)
 	return myTransform
 
 
 class RandomRotate(FilterWithDialog):
-	
+
 	# Definitions of IBOutlets
 	dialog = objc.IBOutlet()
 	maxAngleField = objc.IBOutlet()
@@ -80,7 +79,7 @@ class RandomRotate(FilterWithDialog):
 			'es': 'Rotación aleatoria',
 			'fr': 'Rotation aléatoire',
 		})
-		
+
 		# Word on Run Button (default: Apply)
 		self.actionButtonLabel = Glyphs.localize({
 			'en': 'Rotate',
@@ -88,40 +87,37 @@ class RandomRotate(FilterWithDialog):
 			'es': 'Rotar',
 			'fr': 'Tourner',
 		})
-		
+
 		# Load dialog from .nib (without .extension)
 		self.loadNib('IBdialog', __file__)
-
 
 	# On dialog show
 	@objc.python_method
 	def start(self):
-		
+
 		# Set default value
 		Glyphs.registerDefault(self.prefDomain('maxAngle'), 15.0)
-		if self.pref('maxAngle') == "GlyphsToolHand": # circumvent bug in API
+		if self.pref('maxAngle') == "GlyphsToolHand":  # circumvent bug in API
 			self.pref('maxAngle') = 15.0
-		
+
 		# Set value of text field
 		self.maxAngleField.setStringValue_(self.pref('maxAngle'))
-		
+
 		# Set focus to text field
 		self.maxAngleField.becomeFirstResponder()
-
 
 	# Action triggered by UI
 	@objc.IBAction
 	def setMaxAngle_(self, sender):
 		# Store value coming in from dialog
-		self.pref('maxAngle') = sender.floatValue()
+		self.pref('maxAngle') = 15.0
 		# Trigger redraw
 		self.update()
-
 
 	# Actual filter
 	@objc.python_method
 	def filter(self, layer, inEditView, customParameters):
-		
+
 		# Called on font export, get value from customParameters
 		if 'maxAngle' in customParameters:
 			maxAngle = customParameters['maxAngle']
@@ -133,7 +129,7 @@ class RandomRotate(FilterWithDialog):
 		# fallback to default
 		else:
 			maxAngle = 15.0
-		
+
 		rotationAngle = -maxAngle + 2 * maxAngle * random()
 		centerPoint = centerOfRect(layer.bounds)
 		rotateLayerAroundItsCenter = transform(shiftX=-centerPoint.x, shiftY=-centerPoint.y)
